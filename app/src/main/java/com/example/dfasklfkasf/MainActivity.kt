e302c04f861c1c7c0ad7d526749362341b76dd92
Ayxan <ayxanasgarli4@gmail.com>
2026-05-21 16:24:48 +0400

    second commit

--- app/src/main/java/com/example/dfasklfkasf/MainActivity.kt
+++ app/src/main/java/com/example/dfasklfkasf/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.example.dfasklfkasf
 
 import android.os.Bundle
+import android.renderscript.ScriptGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.dfasklfkasf.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.sum.setOnClickListener {
+            val a = binding.editTextText.text.toString().toIntOrNull()
+            val b = binding.editTextText2.text.toString().toIntOrNull()
+
+            if (a != null && b != null) {
+                val cem = a + b
+                binding.textView.text = cem.toString()
+            } else {
+                binding.textView.text = "Duzgfun daxil et"
+            }
+
+            }
+        binding.minus.setOnClickListener {
+            val a = binding.editTextText.text.toString().toIntOrNull()
+            val b = binding.editTextText2.text.toString().toIntOrNull()
+
+            if (a != null && b != null) {
+                val minus = a - b
+                binding.textView.text = minus.toString()
+            } else {
+                binding.textView.text = "Duzgfun daxil et"
+            }
         }
-    }
-}
+    }}
